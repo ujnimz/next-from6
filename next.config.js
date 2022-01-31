4a690a6d687f0dcc61629c3f1a745dfe9e5fcb5b
Ujith Nimantha <ujithnimantha@gmail.com>
2022-01-31 11:13:51 +0300

    ready for vercel

--- next.config.js
+++ next.config.js
@@ -3,11 +3,11 @@ module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
   images: {
-    //loader: 'imgix',
-    //path: 'https://from6.imgix.net',
+    // loader: 'imgix',
+    // path: 'https://froms.imgix.net',
     // loader: 'cloudinary',
     // path: 'https://from6-com.mo.cloudinary.net',
-    domains: ['localhost', 'from6-com.mo.cloudinary.net', 'from6.imgix.net'],
+    domains: ['localhost', 'from6-com.mo.cloudinary.net', 'froms.imgix.net'],
   },
   // i18n: {
   //   locales: ['en', 'ar'],
