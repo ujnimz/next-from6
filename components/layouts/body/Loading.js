dbcca933ad8a57a559b0a74fb783a85779f2a69b
Ujith Nimantha <ujithnimantha@gmail.com>
2022-02-27 16:07:05 +0300

    Loading screen updated

--- components/layouts/body/Loading.js
+++ components/layouts/body/Loading.js
@@ -2,7 +2,7 @@ import Logo from '../../elements/Logo';
 
 const Loading = () => {
   return (
-    <div className='h-screen w-full flex justify-center items-center'>
+    <div className='h-screen w-full flex justify-center items-center bg-base-100'>
       <Logo />
     </div>
   );
