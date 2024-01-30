d0be5ea2ab0f9ab878fd870a17159119ba692363
ujithdon <udon@tra.org.bh>
2024-01-30 14:57:43 +0300

    Error handling added to apolloCon

--- con/apolloCon.js
+++ con/apolloCon.js
@@ -1,4 +1,43 @@
-import {ApolloClient, InMemoryCache} from '@apollo/client';
+import {
+  from,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  HttpLink,
+  ApolloLink,
+} from '@apollo/client';
+import {setContext} from '@apollo/client/link/context';
+import {onError} from '@apollo/client/link/error';
+
+// Log any GraphQL errors or network error that occurred
+const errorLink = onError(({graphQLErrors, networkError}) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({message, locations, path}) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const httpLink = new HttpLink({
+  uri: `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/graphql`,
+});
+
+const authLink = new ApolloLink((operation, forward) => {
+  // Retrieve the authorization token from local storage.
+  const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
+
+  // Use the setContext method to set the HTTP headers.
+  operation.setContext({
+    headers: {
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  });
+
+  // Call the next link in the middleware chain.
+  return forward(operation);
+});
 
 const defaultOptions = {
   watchQuery: {
@@ -12,7 +51,7 @@ const defaultOptions = {
 };
 
 export const apolloCon = new ApolloClient({
-  uri: `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/graphql`,
+  link: from([errorLink, authLink.concat(httpLink)]),
   cache: new InMemoryCache(),
   defaultOptions,
 });
